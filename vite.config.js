3bd1cb9349502ca94d41d489b3465d9687257ea4
Nimesh Kadecha <nimesh@weybee.in>
2025-07-25 15:59:26 +0530

    adding reander webserver api

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "https://react-todo-1-ugbv.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
